665091e890f7a8402cfbec9b60cb4b953c2d013b
jyfocus <997846481@qq.com>
2018-04-14 17:06:10 +0800

    checkin

--- chin.py
+++ chin.py
@@ -1,7 +1,7 @@
 import requests
 
 s=requests.session()
-data={'email':'997846481@qq.com','passwd':'shadow2000'}
+data={'email':'1018667195@qq.com','passwd':'shadow2000'}
 r=s.post('http://www.yesssr.com/auth/login',data=data)
 rch=s.post('http://www.yesssr.com/user/checkin')
 print(rch.text)
