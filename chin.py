ddcffdfc4a82883d3c9813d9bab093330b5bb816
jyfocus <997846481@qq.com>
2018-09-24 08:45:01 +0800

    Update chin.py

--- chin.py
+++ chin.py
@@ -2,6 +2,6 @@ import requests
 
 s=requests.session()
 data={'email':'1018667195@qq.com','passwd':'shadow2000'}
-r=s.post('http://www.yesssr.com/auth/login',data=data)
-rch=s.post('http://www.yesssr.com/user/checkin')
+r=s.post('https://www.yesssr.com/auth/login',data=data)
+rch=s.post('https://www.yesssr.com/user/checkin')
 print(rch.text)
